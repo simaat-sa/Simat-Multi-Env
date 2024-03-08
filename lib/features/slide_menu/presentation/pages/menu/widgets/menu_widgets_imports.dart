e5cb65feebb73d46e328abcee7becadbd786ffb2
MohamedElsaied20 <mohamed.elsaid@simaat.sa>
2024-03-09 01:54:11 +0400

    add saedan env

--- lib/features/slide_menu/presentation/pages/menu/widgets/menu_widgets_imports.dart
+++ lib/features/slide_menu/presentation/pages/menu/widgets/menu_widgets_imports.dart
@@ -1,3 +1,4 @@
+import 'package:app_settings/app_settings.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_tdd/core/helpers/di.dart';
 import 'package:flutter_tdd/core/helpers/services/current_version_helper.dart';
